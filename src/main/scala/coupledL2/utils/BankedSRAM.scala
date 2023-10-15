b2788812a862b6fa7fa037e159ad6e7001750708
Archer613 <2846509110@qq.com>
2023-10-16 02:57:30 +0800

    Fix PopCount(ren_vec) > 1

--- src/main/scala/coupledL2/utils/BankedSRAM.scala
+++ src/main/scala/coupledL2/utils/BankedSRAM.scala
@@ -58,20 +58,31 @@ class BankedSRAM[T <: Data]
   }.unzip
 
 
-  val ren_vec_0 = VecInit(banks.map(_.io.r.req.fire))
-  val ren_vec_1 = RegNext(ren_vec_0, 0.U.asTypeOf(ren_vec_0))
-  val ren_vec = if(clk_div_by_2){
-    RegNext(ren_vec_1, 0.U.asTypeOf(ren_vec_0))
-  } else ren_vec_1
+  // resp data sel
+  val ren_sel_0 = UIntToOH(r_bankSel)
+  val ren_sel_1 = RegNext(ren_sel_0, 0.U.asTypeOf(ren_sel_0))
+  val ren_sel = if(clk_div_by_2){
+    RegNext(ren_sel_1, 0.U.asTypeOf(ren_sel_0))
+  } else ren_sel_1
+  // resp data valid
+  val resp_data_valid_0 = Mux1H(ren_sel_0, banks.map(_.io.r.req.fire))
+  val resp_data_valid_1 = RegNext(resp_data_valid_0, false.B)
+  val resp_data_valid = if(clk_div_by_2){
+    RegNext(resp_data_valid_1, false.B)
+  } else resp_data_valid_1
 
   // only one read/write
-  assert({PopCount(ren_vec) <= 1.U})
-  assert({PopCount(Cat(banks.map(_.io.r.req.fire))) <= 1.U})
+  // assert({PopCount(ren_vec) <= 1.U})
+  // assert({PopCount(Cat(banks.map(_.io.r.req.fire))) <= 1.U})
 
   // block read when there is write request of the same bank
   io.r.req.ready := Cat(banks.map(s => s.io.r.req.ready && !s.io.w.req.valid).reverse)(r_bankSel)
   // TODO: r.ready low when w.valid is also guaranteed in SRAMTemplate
-  io.r.resp.data := Mux1H(ren_vec, banks.map(_.io.r.resp.data))
+  val r_resp_data = WireInit(0.U.asTypeOf(io.r.resp.data))
+  when(resp_data_valid){
+    r_resp_data := Mux1H(ren_sel, banks.map(_.io.r.resp.data))
+  }
+  io.r.resp.data := r_resp_data
 
   io.w.req.ready := Cat(banks.map(_.io.w.req.ready).reverse)(w_bankSel)
 
