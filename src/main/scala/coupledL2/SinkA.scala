54e27ef5db09ad7334aab06b44b7f20d4f2caa97
qminhao <huanghualiwood@163.com>
2023-12-01 15:25:34 +0800

    previous commit compile wrong ,use this one

--- src/main/scala/coupledL2/SinkA.scala
+++ src/main/scala/coupledL2/SinkA.scala
@@ -113,10 +113,9 @@ class SinkA(implicit p: Parameters) extends L2Module with HasPerfLogging{
   if (prefetchOpt.nonEmpty) {
     val pipe = Module(new Pipeline(io.prefetchReq.get.bits.cloneType, 1))
     pipe.io.in <> io.prefetchReq.get
-    pipe.io.in.ready := true.B
     prefetchReq.get.valid := pipe.io.out.valid
     prefetchReq.get.bits := fromPrefetchReqtoTaskBundle(pipe.io.out.bits)
-    pipe.io.out.ready := prefetchReq.get.ready
+    io.prefetchReq.get.ready := true.B
     fastArb(Seq(commonReq, prefetchReq.get), io.task)
   } else {
     io.task <> commonReq
