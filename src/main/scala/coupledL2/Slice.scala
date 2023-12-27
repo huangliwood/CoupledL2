dc389f60b9888411133e918358f19056423e79af
Archer613 <2846509110@qq.com>
2023-12-27 17:35:30 +0800

    Add status_vec_toC in mainPipe_io

--- src/main/scala/coupledL2/Slice.scala
+++ src/main/scala/coupledL2/Slice.scala
@@ -138,9 +138,9 @@ class Slice(parentName:String = "Unknown")(implicit p: Parameters) extends L2Mod
 
   grantBuf.io.d_task <> mainPipe.io.toSourceD
   grantBuf.io.fromReqArb.status_s1 := reqArb.io.status_s1
-  grantBuf.io.pipeStatusVec := reqArb.io.status_vec ++ mainPipe.io.status_vec
+  grantBuf.io.pipeStatusVec := reqArb.io.status_vec ++ mainPipe.io.status_vec_toD
   mshrCtl.io.pipeStatusVec(0) := reqArb.io.status_vec(1) // s2 status
-  mshrCtl.io.pipeStatusVec(1) := mainPipe.io.status_vec(0) // s3 status
+  mshrCtl.io.pipeStatusVec(1) := mainPipe.io.status_vec_toD(0) // s3 status
 
   if(io.prefetch.isDefined){
     val pfio = io.prefetch.get
