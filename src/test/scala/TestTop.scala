9628766d7b4c510ae7075435923fcf344214e953
Archer613 <2846509110@qq.com>
2024-01-04 17:59:19 +0800

    TestTop: rename l2node -> l2

--- src/test/scala/TestTop.scala
+++ src/test/scala/TestTop.scala
@@ -511,7 +511,7 @@ class TestTop_fullSys()(implicit p: Parameters) extends LazyModule {
     master_nodes = master_nodes ++ Seq(l1d, l1i) // TODO
 
     val l1xbar = TLXbar()
-    val l2node = LazyModule(new CoupledL2()(new Config((_, _, _) => {
+    val l2 = LazyModule(new CoupledL2()(new Config((_, _, _) => {
       case L2ParamKey => L2Param(
         name = s"l2$i",
         ways = 4,
@@ -540,14 +540,14 @@ class TestTop_fullSys()(implicit p: Parameters) extends LazyModule {
     })))
     l1xbar := TLBuffer() := l1i
     l1xbar := TLBuffer() := l1d
-    l2node.pf_recv_node match{
+    l2.pf_recv_node match{
       case Some(l2Recv) => 
         val l1_sms_send_0_node = LazyModule(new PrefetchSmsOuterNode)
         l2Recv := l1_sms_send_0_node.outNode
       case None =>
     }
-    l2xbar := TLBuffer() := l2node.node := l1xbar
-    l2node // return l2 list
+    l2xbar := TLBuffer() := l2.node := l1xbar
+    l2 // return l2 list
   }
 
   val l3 = LazyModule(new HuanCun()(new Config((_, _, _) => {
