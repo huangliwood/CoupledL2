4efd99c7800654b7e6456326733aeb2cda5fd364
cyril0124 <211998078@qq.com>
2024-01-16 17:07:50 +0800

    TestTop4Core will generate splited RTL

--- src/test/scala/TestTop4Core.scala
+++ src/test/scala/TestTop4Core.scala
@@ -347,6 +347,8 @@ object TestTop_fullSys_4Core extends App {
       " disallowPortDeclSharing, disallowLocalVariables," +
       " emittedLineLength=120, explicitBitcast, locationInfoStyle=plain," +
       " disallowExpressionInliningInPorts, disallowMuxInlining"),
+    FirtoolOption("--split-verilog"),
+    FirtoolOption("-o=./build/rtl"),
     ChiselGeneratorAnnotation(() => top.module),
   ))
 }
